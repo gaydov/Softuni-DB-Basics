194ab87e338b17d4c99c81127a38cb1b8607a230
gaydov <gaidov@gmail.com>
2017-09-30 11:36:56 +0300

    add task

--- 7JoinsSubqueriesIndexesCTE/7JoinsSubqueriesIndexesCTE/Geography DB queries.sql
+++ 7JoinsSubqueriesIndexesCTE/7JoinsSubqueriesIndexesCTE/Geography DB queries.sql
@@ -48,3 +48,15 @@ LEFT OUTER JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
 LEFT OUTER JOIN Rivers AS r ON cr.RiverId = r.Id
 GROUP BY c.CountryName
 ORDER BY [HighestPeakElevation] DESC, [LongestRiverLength] DESC, c.CountryName
+
+-- *Highest Peak Name and Elevation by Country
+SELECT TOP (5) WITH TIES c.CountryName, ISNULL(p.PeakName, '(no highest peak)') AS 'HighestPeakName', ISNULL(MAX(p.Elevation), 0) AS 'HighestPeakElevation', ISNULL(m.MountainRange, '(no mountain)')
+FROM Countries AS c
+LEFT JOIN MountainsCountries AS mc
+ON c.CountryCode = mc.CountryCode
+LEFT JOIN Mountains AS m
+ON mc.MountainId = m.Id
+LEFT JOIN Peaks AS p
+ON m.Id = p.MountainId
+GROUP BY c.CountryName, p.PeakName, m.MountainRange
+ORDER BY c.CountryName, p.PeakName
