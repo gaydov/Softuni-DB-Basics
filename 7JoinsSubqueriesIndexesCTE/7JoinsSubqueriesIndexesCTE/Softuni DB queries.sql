86c655671394c5743b570d86b957d519be76cb00
gaydov <gaidov@gmail.com>
2017-10-10 21:40:52 +0300

    capitalize a keyword

--- 7JoinsSubqueriesIndexesCTE/7JoinsSubqueriesIndexesCTE/Softuni DB queries.sql
+++ 7JoinsSubqueriesIndexesCTE/7JoinsSubqueriesIndexesCTE/Softuni DB queries.sql
@@ -30,7 +30,7 @@ ORDER BY d.DepartmentID
 SELECT TOP(3) e.EmployeeID, e.FirstName
 FROM Employees AS e
 LEFT OUTER JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
-WHERE ep.ProjectID IS null
+WHERE ep.ProjectID IS NULL
 ORDER BY e.EmployeeID
 
 -- Employees Hired After
