9702d1396b5cc75460cda10fc695b4c9fb5d7583
gaydov <gaidov@gmail.com>
2017-10-11 21:31:07 +0300

    remove the CASE WHEN from the second grouping

--- 5DataAggregation/5DataAggregation/Gringotts DB queries.sql
+++ 5DataAggregation/5DataAggregation/Gringotts DB queries.sql
@@ -52,27 +52,21 @@ FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 
 -- Age Groups
-SELECT 
-	CASE
-	  WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-	  WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
-	  WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-	  WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-	  WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-	  WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-	  WHEN Age >= 61 THEN '[61+]'
-	END AS [AgeGroup],
-COUNT(*) AS [WizardCount]
-	FROM WizzardDeposits
-GROUP BY CASE
-	  WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-	  WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
-	  WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-	  WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-	  WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-	  WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-	  WHEN Age >= 61 THEN '[61+]'
-	END
+SELECT grouped.AgeGroup, COUNT(*) AS WizzardCount 
+FROM
+	(SELECT 
+		CASE
+		  WHEN wd.Age BETWEEN 0 AND 10 THEN '[0-10]'
+		  WHEN wd.Age BETWEEN 11 AND 20 THEN '[11-20]'
+		  WHEN wd.Age BETWEEN 21 AND 30 THEN '[21-30]'
+		  WHEN wd.Age BETWEEN 31 AND 40 THEN '[31-40]'
+		  WHEN wd.Age BETWEEN 41 AND 50 THEN '[41-50]'
+		  WHEN wd.Age BETWEEN 51 AND 60 THEN '[51-60]'
+		  WHEN wd.Age >= 61 THEN '[61+]'
+		END AS [AgeGroup]
+	FROM WizzardDeposits AS wd
+	) AS grouped
+GROUP BY grouped.AgeGroup
 
 -- First Letter
 SELECT LEFT(FirstName, 1) AS FirstLetter
