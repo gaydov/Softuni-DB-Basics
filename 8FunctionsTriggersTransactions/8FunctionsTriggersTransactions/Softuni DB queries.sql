48524ac76dda46fd8b62514792a803b8e77497a4
gaydov <gaidov@gmail.com>
2017-10-13 21:00:30 +0300

    add one more task

--- 8FunctionsTriggersTransactions/8FunctionsTriggersTransactions/Softuni DB queries.sql
+++ 8FunctionsTriggersTransactions/8FunctionsTriggersTransactions/Softuni DB queries.sql
@@ -156,3 +156,22 @@ VALUES
 (@emloyeeId, @projectID)
 
 COMMIT
+
+-- Delete Employees
+CREATE TABLE Deleted_Employees
+(
+EmployeeId int PRIMARY KEY IDENTITY,
+FirstName varchar(50),
+LastName varchar(50),
+MiddleName varchar(50),
+JobTitle varchar(50),
+DepartmentId int,
+Salary money
+)
+
+GO
+CREATE TRIGGER tr_DeleteEmployee ON Employees AFTER DELETE
+AS
+INSERT INTO Deleted_Employees(FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+SELECT d.FirstName, d.LastName, d.MiddleName, d.JobTitle, d.DepartmentID, d.Salary 
+FROM deleted AS d
