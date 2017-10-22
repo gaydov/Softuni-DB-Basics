ad9a8eed008fd1c6488e57b4f8bd218f48512d2a
gaydov <gaidov@gmail.com>
2017-10-22 18:05:31 +0300

    remove spaces

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/18. Assign Employee.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/18. Assign Employee.sql
@@ -3,11 +3,11 @@ AS
 BEGIN TRANSACTION
 
 	DECLARE @employeeDepartmentId int = (SELECT DepartmentId
-					     FROM   Employees
-					     WHERE  Id = @employeeId);
+					     FROM Employees
+					     WHERE Id = @employeeId);
  					   
 	DECLARE @reportDepartmentId int = (SELECT c.DepartmentId
-				           FROM   Reports AS r
+				           FROM Reports AS r
 				           INNER JOIN Categories AS c ON c.Id = r.CategoryId
 				           WHERE r.Id = @reportId)
 
