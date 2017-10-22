b9ff49636fb059ad8a4a05f7e06781855d234aeb
gaydov <gaidov@gmail.com>
2017-10-22 17:58:26 +0300

    remove spaces

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/16. Favorite Categories.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/16. Favorite Categories.sql
@@ -1,15 +1,16 @@
 WITH CTE_ReportsByDepartment
 AS
-(	SELECT d.[Name] AS DeptName, c.[Name] AS CatName, COUNT(c.[Name]) as [CountReports]
-	FROM Departments AS d
-	INNER JOIN Categories AS c ON c.DepartmentId = d.Id
-	INNER JOIN Reports AS r ON r.CategoryId = c.Id
-	GROUP BY d.[Name] , c.[Name] 
+(
+SELECT d.[Name] AS DeptName, c.[Name] AS CatName, COUNT(c.[Name]) as [CountReports]
+FROM Departments AS d
+INNER JOIN Categories AS c ON c.DepartmentId = d.Id
+INNER JOIN Reports AS r ON r.CategoryId = c.Id
+GROUP BY d.[Name] , c.[Name] 
 )
 
 SELECT d.[Name] AS [Department Name], 
-	   c.[Name] AS [Category Name], 
-	   ROUND((CAST(COUNT(c.[Name]) AS FLOAT) / CAST((SELECT SUM([CountReports]) FROM CTE_ReportsByDepartment WHERE DeptName = d.[Name]) AS FLOAT)) * 100, 0) AS [Percentage]
+	  c.[Name] AS [Category Name], 
+	  ROUND((CAST(COUNT(c.[Name]) AS FLOAT) / CAST((SELECT SUM([CountReports]) FROM CTE_ReportsByDepartment WHERE DeptName = d.[Name]) AS FLOAT)) * 100, 0) AS [Percentage]
 FROM Departments AS d
 INNER JOIN Categories AS c ON c.DepartmentId = d.Id
 INNER JOIN Reports AS r ON r.CategoryId = c.Id
