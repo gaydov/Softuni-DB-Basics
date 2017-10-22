58553c470fe4484ff1d962f922028d5c0883bb1d
gaydov <gaidov@gmail.com>
2017-10-22 18:06:03 +0300

    remove spaces

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/19. Close Reports.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/19. Close Reports.sql
@@ -1,11 +1,11 @@
 CREATE TRIGGER TR_CloseReports ON Reports AFTER UPDATE
 AS
 	DECLARE @completedStatusId int = (SELECT Id
-									  FROM   [Status]
-									  WHERE  Label = 'completed')
+						FROM   [Status]
+						WHERE  Label = 'completed')
 
 	UPDATE Reports
 	SET StatusId = @completedStatusId
 	WHERE Id = (SELECT Id
-				FROM   inserted AS i
-				WHERE  CloseDate IS NOT NULL)
+		FROM   inserted AS i
+		WHERE  CloseDate IS NOT NULL)
