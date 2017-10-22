2e28c94712bcd5bb206396d29932748d4ca13409
gaydov <gaidov@gmail.com>
2017-10-22 17:59:28 +0300

    remove spaces

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/16. Favorite Categories.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/16. Favorite Categories.sql
@@ -1,12 +1,10 @@
 WITH CTE_ReportsByDepartment
 AS
-(
-	SELECT d.[Name] AS DeptName, c.[Name] AS CatName, COUNT(c.[Name]) as [CountReports]
-	FROM Departments AS d
-	INNER JOIN Categories AS c ON c.DepartmentId = d.Id
-	INNER JOIN Reports AS r ON r.CategoryId = c.Id
-	GROUP BY d.[Name] , c.[Name] 
-)
+(SELECT d.[Name] AS DeptName, c.[Name] AS CatName, COUNT(c.[Name]) as [CountReports]
+FROM Departments AS d
+INNER JOIN Categories AS c ON c.DepartmentId = d.Id
+INNER JOIN Reports AS r ON r.CategoryId = c.Id
+GROUP BY d.[Name] , c.[Name])
 
 SELECT	d.[Name] AS [Department Name], 
 	c.[Name] AS [Category Name], 
