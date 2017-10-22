c7288f5120dd4d3d5d27d64def5ebd92317c2807
gaydov <gaidov@gmail.com>
2017-10-22 22:32:57 +0300

    remove unnecessary ISNULL

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/14. Open-Closed Statistics.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/14. Open-Closed Statistics.sql
@@ -1,5 +1,4 @@
- -- NOT TESTED in Judge
- SELECT [EmpName],
+SELECT [EmpName],
         CONCAT(Closed, '/', Opened) AS [Closed Open Reports]
   FROM (SELECT 
 		CONCAT(e.FirstName, ' ' ,e.LastName) AS [EmpName],
@@ -7,10 +6,10 @@
 		COUNT(r.CloseDate) AS Closed,
 		COUNT(r.OpenDate) AS Opened
 		FROM Employees AS e
-		JOIN Reports AS r
+		INNER JOIN Reports AS r
 		ON r.EmployeeId = e.Id
 		WHERE DATEPART(year, r.OpenDate) = 2016
-		OR ISNULL(DATEPART(year, r.CloseDate), 0) = 2016
+		OR DATEPART(year, r.CloseDate) = 2016
 		GROUP BY CONCAT(e.FirstName, ' ', e.LastName), e.Id) AS epcount
 INNER JOIN Employees AS e ON e.Id = epcount.EmpId
 ORDER BY [EmpName], e.Id
