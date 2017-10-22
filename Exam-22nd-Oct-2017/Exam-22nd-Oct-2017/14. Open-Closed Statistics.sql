73c3f023089b1e90beb0ebe343aedad784cecc7c
gaydov <gaidov@gmail.com>
2017-10-22 17:55:10 +0300

    remove spaces

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/14. Open-Closed Statistics.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/14. Open-Closed Statistics.sql
@@ -1,7 +1,7 @@
  -- NOT TESTED in Judge
  SELECT [EmpName],
         CONCAT(Closed, '/', Opened) AS [Closed Open Reports]
-  FROM (SELECT	CONCAT(e.FirstName, ' ' ,e.LastName) AS [EmpName],
+  FROM (SELECT CONCAT(e.FirstName, ' ' ,e.LastName) AS [EmpName],
 				e.Id AS EmpId,
 				COUNT(r.CloseDate) AS Closed,
 				COUNT(r.OpenDate) AS Opened
