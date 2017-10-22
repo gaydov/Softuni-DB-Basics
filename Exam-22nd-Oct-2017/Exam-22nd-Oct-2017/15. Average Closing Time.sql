3f2ca50dbd4f344c0b10c905ee64796e4a232083
gaydov <gaidov@gmail.com>
2017-10-22 17:56:25 +0300

    remove spaces

--- Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/15. Average Closing Time.sql
+++ Exam-22nd-Oct-2017/Exam-22nd-Oct-2017/15. Average Closing Time.sql
@@ -1,5 +1,6 @@
-SELECT d.[Name] AS [Department Name], 
-	   ISNULL(CONVERT(VARCHAR(10), AVG(DATEDIFF(day, r.OpenDate, r.CloseDate))), 'no info') AS [Average Duration]
+SELECT 
+		d.[Name] AS [Department Name], 
+		ISNULL(CONVERT(VARCHAR(10), AVG(DATEDIFF(day, r.OpenDate, r.CloseDate))), 'no info') AS [Average Duration]
 FROM Departments AS d
 INNER JOIN Categories AS c ON c.DepartmentId = d.Id
 INNER JOIN Reports AS r ON r.CategoryId = c.Id
